16089a4859dce543d94fcdc391bed2f12233a600
Nate St. George <nateestes223@gmail.com>
2021-12-12 15:47:03 -0700

    Refactor to prepare for web version

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-
 import cv2
 import numpy as np
 from tqdm import tqdm
@@ -12,7 +11,7 @@ class Framer:
         self.__current_frame = 0
         self.__frame_count = self.__get_frame_count()
         self.__x = int(dim[0]) if int(dim[0]) != 0 else self.__frame_count
-        self.__y = int(dim[1])
+        self.__y = int(dim[1]) if int(dim[1]) != 0 else int(self.__frame_count / 5)
 
         self.__capture = cv2.VideoCapture(path)
 
@@ -21,9 +20,6 @@ class Framer:
                 "Failed to read video file. Is the path correct? If so, check that OpenCV has the codecs required to "
                 "read this file.")
             exit(1)
-
-        print("Beginning generation on {} ({} frames)...".format(self.__path, self.__frame_count))
-        self.generate()
 
     def __get_frame_count(self):
         """Get the number of frames in the video either using the metadata or counting manually."""
@@ -53,6 +49,7 @@ class Framer:
 
     def generate(self):
         """Generate the image."""
+        print("Beginning generation on {} ({} frames)...".format(self.__path, self.__frame_count))
         mean_colors = np.empty((self.__frame_count, 1, 3))
         for i in tqdm(range(self.__frame_count)):
             success, frame = self.__read_next_frame()
@@ -63,21 +60,24 @@ class Framer:
 
         # Make the strip horizontal, then resize to the user's expected size
         mean_colors = cv2.rotate(mean_colors, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        mean_colors = cv2.resize(mean_colors, (self.__x, self.__y))
-
-        if cv2.imwrite(self.__output, mean_colors):
-            print("Saved to {}".format(self.__output))
-        else:
-            print("File not saved. Check permissions for the location you're trying to save to.")
+        return cv2.resize(mean_colors, (self.__x, self.__y))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a strip of colors for each frame in a video file.")
     parser.add_argument("path", help="The path to the video file.")
     parser.add_argument("output", help="Output file name.")
-    parser.add_argument("size", help="Dimensions of the output file, {x}x{y}. If the x value is 0, it will "
-                                     "automatically be 1 pixel per frame.")
+    parser.add_argument("size", help="Dimensions of the output file, XxY (like 2000x400). "
+                                     "If the X value is 0, it will automatically be 1 pixel per frame. "
+                                     "If Y is also 0, it will default to a 5:1 aspect ratio.")
 
     args = parser.parse_args()
 
     framer = Framer(args.path, args.output, args.size.split("x"))
+    result = framer.generate()
+
+    if cv2.imwrite(args.output, result):
+        print("Saved to {}".format(args.output))
+    else:
+        print("File not saved. Check permissions for the location you're trying to save to.")
+
